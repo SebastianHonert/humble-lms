2d714a8b45b0f82ce85cfa96445dc76823f48f95
Sebastian Honert <sebastian.honert@gmail.com>
2020-06-03 16:24:10 +0200

    Track: single view

--- public/partials/humble-lms-track-single.php
+++ public/partials/humble-lms-track-single.php
@@ -1,14 +1,29 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) )
-  exit;
-
 get_header();
 
-echo '<h1 class="humble-lms-track-single-title">' . get_the_title() . '</h1>';
+if (have_posts()):
+  
+  while (have_posts()): the_post();
 
-echo do_shortcode('[humble_lms_paypal_buttons_single_item]');
+    echo '<section id="humble-lms-track-content">';
 
-echo do_shortcode('[humble_lms_course_archive track_id="' . get_the_ID() . '"]');
+      do_action( 'humble_lms_before_track_title' );
+      
+      echo '<h1 class="humble-lms-track-single-title">' . get_the_title() . '</h1>';
+
+      echo do_shortcode('[humble_lms_paypal_buttons_single_item]');
+
+      echo get_the_content();
+
+      do_action( 'humble_lms_after_track_content' );
+
+    echo '</section>';
+
+    echo do_shortcode('[humble_lms_course_archive track_id="' . get_the_ID() . '"]');
+
+  endwhile;
+
+endif;
 
 get_footer();

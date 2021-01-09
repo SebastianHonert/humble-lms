1128d71733c4e1eea20f905e5b0783fb4638b506
Sebastian Honert <sebastian.honert@gmail.com>
2021-01-09 20:16:56 +0100

    0.1.1; Lightbox overflow behavior; i18n

--- includes/class-humble-lms.php
+++ includes/class-humble-lms.php
@@ -70,7 +70,7 @@ class Humble_LMS {
     if ( defined( 'HUMBLE_LMS_VERSION' ) ) {
       $this->version = HUMBLE_LMS_VERSION;
     } else {
-      $this->version = '0.1.0';
+      $this->version = '0.1.1';
     }
     $this->humble_lms = 'humble-lms';
 

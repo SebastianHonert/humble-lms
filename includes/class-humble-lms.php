da6481af9eebe6fcc3d12f628d8ded3dafd2494c
Sebastian Honert <sebastian.honert@gmail.com>
2021-02-18 13:01:30 +0100

    0.1.5

--- includes/class-humble-lms.php
+++ includes/class-humble-lms.php
@@ -70,7 +70,7 @@ class Humble_LMS {
     if ( defined( 'HUMBLE_LMS_VERSION' ) ) {
       $this->version = HUMBLE_LMS_VERSION;
     } else {
-      $this->version = '0.1.4';
+      $this->version = '0.1.5';
     }
     $this->humble_lms = 'humble-lms';
 
